12afcdfc099c2a406cb36172017c0cd190a9b658
EliHunt98 <50928780+EliHunt98@users.noreply.github.com>
2019-09-23 10:27:39 -0600

    Add files via upload

---
+++ SignalRServer/SignalRServer/Controllers/MessageController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRChat.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SignalRServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        IHubContext<BroadcastHub> messageHubContext;
+        public MessageController(IHubContext<BroadcastHub> messageHubContext)
+        {
+            this.messageHubContext = messageHubContext;
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> Get(string user, string message, string connId)
+        {
+            if (string.IsNullOrEmpty(connId))
+            {
+                await this.messageHubContext.Clients.All.SendAsync("ReceiveMessage", user, message);
+            }
+            else
+            {
+                await this.messageHubContext.Clients.Client(connId).SendAsync("ReceieveMessage", user, message);
+            }
+            return new JsonResult("Message Sent");
+        }
+    }
+}
